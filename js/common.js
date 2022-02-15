ee256ba12c204f53b048085d234a5749f9538a2f
byeolyee <byeolyee2230@gmail.com>
2022-02-16 03:51:49 +0900

    공통 모듈 분리

---
+++ js/common.js
@@ -0,0 +1,19 @@
+// header submenu 검색
+// const { default: Swiper } = require("swiper");
+
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+    searchInputEl.focus();
+})
+
+searchInputEl.addEventListener('focus', function () {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+})
+
+searchInputEl.addEventListener('blur', function () {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '')
+})
