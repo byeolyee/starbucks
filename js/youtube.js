d0f63a913cc944827282cb6b7ca45b0a539d8146
byeolyee <byeolyee2230@gmail.com>
2022-04-13 20:39:10 +0900

    고정이미지배경

--- js/youtube.js
+++ js/youtube.js
@@ -1,12 +1,10 @@
-// 2. This code loads the IFrame Player API code asynchronously.
+
 const tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
 function onYouTubeIframeAPIReady() {
     //<div id="player"></div>
     new YT.Player('player', {
@@ -22,23 +20,4 @@ function onYouTubeIframeAPIReady() {
             }
         }
     });
-}
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-const done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
 }
